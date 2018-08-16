3442fe71040e7e9221a7177ab50719d709a13e71
Jorge Mayoraz <jorge.emh@hotmail.com>
2018-08-16 20:58:49 -0300

    multiple masks

--- iframe_masks.js
+++ iframe_masks.js
@@ -2,33 +2,48 @@ function onLoadMasks() {
     var masks = [];
     var maskedElements = [];
 
-    masks.push("mask-iframe-id");
+    //ids for masks
+    masks.push("mask-iframe-1");
+    masks.push("mask-iframe-2");
 
+    // Classes for animation
     var inClass = "fadeIn";
     var outClass = "fadeOut";
 
     for (var i = 0; i < masks.length; i++) {
         var currentElem = document.getElementById(masks[i]);
         maskedElements.push(currentElem);
-        document.getElementById(masks[i]).addEventListener("click", function() {
+        currentElem.addEventListener("click", function() {
             event.stopPropagation();
-            OnElementClick(currentElem);
+            OnElementClick(this);
         });
     }
-
     function OnElementClick(elem) {
         elem.classList.remove(inClass);
         elem.classList.add(outClass);
+        var closeButton = document.getElementById(elem.id + "-close");
+        if (closeButton !== null) {
+            closeButton.classList.add(inClass);
+            closeButton.classList.remove(outClass);
+        }
     }
 
     window.addEventListener("click", function() {
         for (var i = 0; i < maskedElements.length; i++) {
             maskedElements[i].classList.add(inClass);
             maskedElements[i].classList.remove(outClass);
+            var closeButton = document.getElementById(
+                maskedElements[i].id + "-close"
+            );
+            if (closeButton !== null) {
+                closeButton.classList.remove(inClass);
+                closeButton.classList.add(outClass);
+            }
         }
     });
 }
 
+//Making sure that the HTML is loaded
 if (window.attachEvent) {
     window.attachEvent("onload", onLoadMasks);
 } else {
