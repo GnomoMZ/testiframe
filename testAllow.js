2ceedee4d77bcb080c69de8e11e61bdbde5a4021
Jorge Mayoraz <jorge.emh@hotmail.com>
2018-08-23 13:35:41 -0300

    test2

---
+++ testAllow.js
@@ -0,0 +1,23 @@
+function onLoadTest() {
+    console.log("aca");
+            var currentElem = document.getElementById("iframe-id-1");
+    console.log(currentElem);
+    currentElem.setAttribute('allow', 'gyroscope; accelerometer');
+
+}
+
+//Making sure that the HTML is loaded
+if (window.attachEvent) {
+    window.attachEvent("onload", onLoadTest);
+} else {
+    if (window.onload) {
+        var curronload = window.onload;
+        var newonload = function(evt) {
+            curronload(evt);
+            onLoadTest(evt);
+        };
+        window.onload = newonload;
+    } else {
+        window.onload = onLoadTest;
+    }
+}
