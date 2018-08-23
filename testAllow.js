93d1f805e32277b088038c7adfa1a13a823afe2c
Jorge Mayoraz <jorge.emh@hotmail.com>
2018-08-23 16:07:02 -0300

    3

--- testAllow.js
+++ testAllow.js
@@ -1,7 +1,32 @@
 function onLoadTest() {
-            var currentElem = document.getElementById("iframe-id-1");
-//    currentElem.setAttribute('allow', 'gyroscope; accelerometer');
+    window.addEventListener("deviceorientation", function(event) {
+        console.log("deviceorientation");
 
+        var iframeElement = document.getElementById("iframe-id-1");
+        iframeElement.contentWindow.postMessage(
+            {
+                alpha: event.alpha,
+                beta: event.beta,
+                gamma: event.gamma
+            },
+            "*"
+        );
+    });
+
+    window.addEventListener(
+        "orientationchange",
+        function() {
+            console.log("orientationchange");
+            var iframe = document.getElementById("iframe-id-1").contentWindow;
+            iframe.postMessage(
+                {
+                    orientation: window.orientation
+                },
+                "*"
+            );
+        },
+        false
+    );
 }
 
 //Making sure that the HTML is loaded
